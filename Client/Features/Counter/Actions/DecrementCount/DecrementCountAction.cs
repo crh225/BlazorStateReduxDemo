2988d804acfbeb4ff7a41cc870f8677c8b5b095f
Chris House <christopher.house@ipaper.com>
2021-09-23 17:49:20 -0500

    add decrement action

--- Client/Features/Counter/Actions/DecrementCount/DecrementCountAction.cs
+++ Client/Features/Counter/Actions/DecrementCount/DecrementCountAction.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 
 namespace BlazorRedux.Client.Features.Counter
 {
+
     using BlazorState;
 
-    public partial class CounterState : State<CounterState>
+    public partial class CounterState
     {
-        public int Count { get; private set; }
-        public override void Initialize() => Count = 1;
+        public class DecrementCountAction : IAction
+        {
+            public int Amount { get; set; }
+        }
     }
 }
---
+++ Client/Features/Counter/Actions/DecrementCount/DecrementCountHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorRedux.Client.Features.Counter
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using BlazorState;
+    using MediatR;
+
+    public partial class CounterState
+    {
+        public class DecrementCountHandler : ActionHandler<DecrementCountAction>
+        {
+            public DecrementCountHandler(IStore aStore) : base(aStore) { }
+
+            CounterState CounterState => Store.GetState<CounterState>();
+
+            public override Task<Unit> Handle(DecrementCountAction aDecrementCountAction, CancellationToken aCancellationToken)
+            {
+                CounterState.Count = CounterState.Count - aDecrementCountAction.Amount;
+                return Unit.Task;
+            }
+        }
+    }
+}
