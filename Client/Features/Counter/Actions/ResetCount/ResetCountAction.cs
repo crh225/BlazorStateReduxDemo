9aecaa9301ae47274330099d61cf62324cd9042c
Chris House <christopher.house@ipaper.com>
2021-09-23 18:07:08 -0500

    reset action

---
+++ Client/Features/Counter/Actions/ResetCount/ResetCountAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRedux.Client.Features.Counter
+{
+
+    using BlazorState;
+
+    public partial class CounterState
+    {
+        public class ResetCountAction : IAction
+        {
+            public int Amount { get; set; }
+        }
+    }
+}
---
+++ Client/Features/Counter/Actions/ResetCount/ResetCountHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorRedux.Client.Features.Counter
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using BlazorState;
+    using MediatR;
+
+    public partial class CounterState
+    {
+        public class ResetCountHandler : ActionHandler<ResetCountAction>
+        {
+            public ResetCountHandler(IStore aStore) : base(aStore) { }
+
+            CounterState CounterState => Store.GetState<CounterState>();
+
+            public override Task<Unit> Handle(ResetCountAction aResetCountAction, CancellationToken aCancellationToken)
+            {
+                CounterState.Count = aResetCountAction.Amount;
+                return Unit.Task;
+            }
+        }
+    }
+}
